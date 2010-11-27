f6c1461ff52e8a77822809f8578192901a6fac18
pupunzi <matteo@open-lab.com>
2010-11-27 01:49:51 +0100

    several improve on buttons and bugfix on double click of a page change link

--- inc/jquery.mb.ile.js
+++ inc/jquery.mb.ile.js
@@ -1,7 +1,7 @@
 /*
  *
  * jQuery.mb.components: jquery.mb.ile
- * version: 0.1 alpha - 12-nov-2010 - 48
+ * version: 1.0 alpha - 12-nov-2010 - 48
  * © 2001 - 2010 Matteo Bicocchi (pupunzi), Open Lab
  *
  * Licences: MIT, GPL
@@ -12,12 +12,12 @@
  * site: http://pupunzi.com
  *
  */
+
 document.iScroll={};
 document.iScroll.enabled=true;
 document.myScroll=null;
 
-
-(function($) {
+(function($){
   $.mbile={
     name:"mb.ile",
     author:"Matteo Bicocchi",
@@ -27,24 +27,23 @@ document.myScroll=null;
       header:"#header",
       footer:"#footer",
       body:"#wrapper",
-      fixedHeader:true,
-      fixedFooter:true,
-      slidingFooter:true,
+      fixedHeaderFooter:true,
+      collapsibleFooter:true,
       startupScreen: false,
       icon:false,
       icon4:false,
-      iconDouble:false,
       addGlossToIcon:false,
       slidingSections:true,
       errorPage:"pages/error_page.html"
     },
     orientation:"portrait",
     pages:{},
+    extensionsInitOnContentLoad:[],
 
-    init:function(options,url){
-      if(typeof options !="object") url=options;
+    init:function(url,options){
       var opt={};
       $.extend(opt,$.mbile.defaults,options);
+
       $.mbile.pages[url]={url:url};
       $.mbile.actualPage=url;
       $.mbile.home=url;
@@ -54,14 +53,15 @@ document.myScroll=null;
       $(opt.header+","+opt.footer).each(function(){
         $(this).wrapInner("<div class='HFcontent'/>");
       });
+
       // Set startup screen
       if (opt.startupScreen)
         $("head").append('<link rel="apple-touch-startup-image" href="' + opt.startupScreen + '" />');
       // Set appropriate icon (retina display stuff is experimental)
       if (opt.icon || opt.icon4) {
         var precomposed, appropriateIcon;
-        if (opt.iconDouble && window.devicePixelRatio && window.devicePixelRatio === 2) {
-          appropriateIcon = opt.iconDouble;
+        if (opt.icon4 && window.devicePixelRatio && window.devicePixelRatio === 2) {
+          appropriateIcon = opt.icon4;
         } else if (opt.icon) {
           appropriateIcon = opt.icon;
         } else {
@@ -73,39 +73,58 @@ document.myScroll=null;
         }
       }
 
-
       /*sliding footer behaviour*/
-      if(opt.slidingFooter){
-        var footerslider= $("<span/>").attr("id","footerslider").attr("collapsed",0).html("&nbsp;");
-        $(opt.footer).append(footerslider);
-        $("#footerslider").bind("mousedown",
-                         function(){
-                           var el=$(this);
-                           if(el.attr("collapsed")==0){
-                             $(opt.footer).addClass("out");
-                             el.attr("collapsed",1)
-                           }else{
-                             $(opt.footer).removeClass("out");
-                             el.attr("collapsed",0)
-                           }
-                         });
-      }
-      
+      if(opt.collapsibleFooter){
+        var collapsibleFooter= $("<span/>").attr("id","collapsibleFooter").attr("collapsed",0).html("&nbsp;");
+        $(opt.footer).append(collapsibleFooter);
+        $(opt.footer).addClass("collapsibleFooter");
 
-      $($.mbile.defaults.body).load( url, function(content){
+        $("#collapsibleFooter")
+            .bind("mousedown",
+                 function(){
+                   var el=$(this);
+                   if(el.attr("collapsed")==0){
+                     $(opt.footer).addClass("out");
+                     el.attr("collapsed",1)
+                   }else{
+                     $(opt.footer).removeClass("out");
+                     el.attr("collapsed",0)
+                   }
+                 });
+      }
+
+      var finalize= function(content){
+
         $(document).bind('orientationchange', $.mbile.checkOrientation);
         $(document).bind('touchmove', function (e) { e.preventDefault(); }, false);
         $.mbile.setHeight();
-
         $.mbile.initContent(opt);
         $(opt.context).css({"visibility":"visible"}).hide();
-
         var event=$.Event("pageshow");
         event.newPage=content;
         $(document).trigger(event);
+        $(opt.context).fadeIn(500);
 
-        $(opt.context).show();
-      });
+      };
+
+      if(url!=undefined && $(url).isOnPage()){
+
+        var pages= $("[data-role=page]");
+        pages.hide();
+        var content=$(url).clone(true);
+        $($.mobile.body).append(content);
+        finalize($(url).get(0));
+        $(url).fadeIn(500);
+
+      }else if(url!=undefined){
+
+        $($.mbile.defaults.body).load(url, function(content){finalize(content);});
+
+      }else{
+
+        alert("Sorry, there's no content to show");
+
+      }
     },
     checkOrientation:function(){
       $.mbile.setHeight();
@@ -119,7 +138,6 @@ document.myScroll=null;
       $($.mbile.defaults.body).css({height:wrapperH-15});
     },
     initContent:function(opt){
-
       if($("#scroller").length==0){
         var scroller=$("<div/>").attr("id","scroller");
         $(opt.body).wrapInner(scroller);
@@ -135,6 +153,7 @@ document.myScroll=null;
         $.mbile.setSortableBehavior();
         $.mbile.setSelectableBehavior();
         $.mbile.setLinkBehavior();
+        $.mbile.setListBehaviour();
         if(opt.slidingSections)
           $.mbile.setSectionBehavior();
         if(document.iScroll.enabled)
@@ -163,7 +182,6 @@ document.myScroll=null;
       var anim= $.mbile.pages[actualPage].anim;
       $(this).addClass("hover").goToPage(url,$.mbile.getBackAnim(anim),false);
 
-      //$(".backBtn").trigger("mousedown");
     },
     setHeaderFooterBehavior:function(opt){
       var newHeader=$(opt.body).find("[data-role=header]").clone(true);
@@ -187,13 +205,15 @@ document.myScroll=null;
       });
       setTimeout($.mbile.setButtonBehavior,100);
     },
-    setListBehaviour:function(){},
+    setListBehaviour:function(){
+      var lines=$(".list .line");
+      lines.setHover();
+    },
     setSelectableBehavior:function(){
       var selectables=$(".line.selectable");
       selectables.each(function(){
         var selImg=$("<span/>").addClass("selImg");
         $(this).append(selImg);
-
 
         $(this).toggle(
                       function(){$(this).addClass("selected")},
@@ -224,7 +244,7 @@ document.myScroll=null;
       var $fakeSection=$("<span>").addClass("fake section").css({position:"absolute", top:0, zIndex:2, width:"100%"}).hide();
       $($.mbile.defaults.body).prepend($fakeSection);
       document.SectionBehavior=setInterval(function(){
-        $sections.each(function () {
+        $sections.each(function(){
           var $section = $(this);
           var top = $section.offset().top;
           if (top < containerTop+10) {
@@ -237,11 +257,8 @@ document.myScroll=null;
       },50)
     },
     setButtonBehavior:function(){
-      var buttons=$("button,.button, .backBtn, .list .line");
-      buttons
-          .bind("mousedown",function(){$(this).addClass("hover")})
-          .bind("mouseup",function(){$(this).removeClass("hover")})
-          .addTouch();
+      var buttons=$("button,.button, .backBtn");
+      buttons.setHover();
     },
     setPanelBehavior:function(){
       var $panels= $($.mbile.defaults.body + " #scroller").find("a[rel=panel]");
@@ -278,7 +295,15 @@ document.myScroll=null;
         }
       });
     },
+    setHover:function(){
+      this.bind("mousedown",function(){$(this).addClass("hover")})
+          .bind("mouseup",function(){$(this).removeClass("hover")})
+          .addTouch();
+    },
     goToPage:function(url,animation,addHistory){
+      if($.mbile.pageIsChanging) return;
+      $.mbile.pageIsChanging=true;
+      
       if(url==$.mbile.actualPage) return;
       /*
        * trigger event: beforepagechange
@@ -310,10 +335,8 @@ document.myScroll=null;
           $(document).stopWait();
           var newPage= $(response).addClass("newPage "+ animation);
           newPage.find("[data-role=header], [data-role=footer]").hide();
-          $($.mbile.defaults.body).append(newPage);
-
           $(".fake").remove();
-
+          $($.mbile.defaults.body).append(newPage);
           /*
            * trigger event: beforepageshow
            *
@@ -333,7 +356,7 @@ document.myScroll=null;
           beforepageshow.oldPage=oldPage;
           $(document).trigger(beforepageshow);
 
-
+          /*Applay transition*/
           oldPage.addClass("out");
           newPage.addClass("in");
           newPage.bind('webkitAnimationEnd', function(){
@@ -342,10 +365,10 @@ document.myScroll=null;
             $("#scroller").remove();
             newPage.removeClass("in "+ animation);
 
-            if(addHistory){
+            if(addHistory)
               $.mbile.pages[url]={url:url, prev:$.mbile.actualPage, anim:animation};
-            }
             $.mbile.actualPage=url;
+
             /*
              * trigger event: pageshow
              *
@@ -368,6 +391,7 @@ document.myScroll=null;
             $(document).trigger(pageshow);
 
             $.mbile.initContent($.mbile.defaults);
+            $.mbile.pageIsChanging=false;            
           });
         }
       });
@@ -440,9 +464,8 @@ document.myScroll=null;
       });
       return loaderScreen;
     },
-    onPageInit:function(callback, e){
-      $(document).one("pageshow",e,callback);
-      //$.mbMobile.onInit=function(){};
+    onPageLoad:function(callback, e){
+      $(document).one("pageshow",e, callback);
     },
     refreshScroll:function(){
       setTimeout(function () { if(document.myScroll) document.myScroll.refresh() }, 0)
@@ -453,6 +476,9 @@ document.myScroll=null;
   $.fn.wait=$.mbile.wait;
   $.fn.stopWait=$.mbile.stopWait;
   $.fn.goToPage=$.mbile.goToPage;
+  $.fn.setHover=$.mbile.setHover;
+
+/*-------------------------------------------------------------------------------------------- Utilities*/
 
   $.fn.openPanel = function(){
     this.removeClass("close");
@@ -465,7 +491,6 @@ document.myScroll=null;
     this.addClass("close").one('webkitAnimationEnd', function(){$(".panel.close").removeClass("close");$.mbile.refreshScroll();});
     this.prev("span").removeClass("selected header");
   };
-
 
   $.fn.swipe =function(opt){
     var defaults={
@@ -497,6 +522,10 @@ document.myScroll=null;
     })
   };
 
+  $.fn.isOnPage=function(){
+    return this.length>0;
+  };
+
 //manage fixed pos for header and footer
   $.fn.setFixed=function(pos){ //top, bottom
     var $el=this;
@@ -521,21 +550,21 @@ document.myScroll=null;
     });
   };
 
-  function setFixedRemove(){
+  $.setFixedRemove=function(){
     $(".fixedPlaceHolder").remove();
-  }
+  };
 
 
+  //on ajax error
   $(function(){
     $(document).bind("ajaxError", function(){
+      $.mbile.pageIsChanging=false;
       $.mbile.goToPage($.mbile.defaults.errorPage,"pop");
     })
   });
 
-  /*
-   function alert(){
-   $.mbMobile.alert()
-   };
-   */
+//  function alert(){
+//    $.mbile.alert()
+//  };
 
 })(jQuery);
