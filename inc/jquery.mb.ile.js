3b90321c5e44195421add98a21e7b1e8a0e11ba7
pupunzi <matteo@open-lab.com>
2013-01-04 17:35:16 +0100

    various

--- inc/jquery.mb.ile.js
+++ inc/jquery.mb.ile.js
@@ -1,29 +1,25 @@
 /*
  * ******************************************************************************
  *  jquery.mb.components
+ *  file: jquery.mb.ile.js
  *
- *  Copyright (c) 2001-2013. Matteo Bicocchi (Pupunzi); Open lab srl, Firenze - Italy
+ *  Copyright (c) 2001-2013. Matteo Bicocchi (Pupunzi);
+ *  Open lab srl, Firenze - Italy
  *  email: matteo@open-lab.com
- *  site: http://pupunzi.com
+ *  site: 	http://pupunzi.com
+ *  blog:	http://pupunzi.open-lab.com
+ * 	http://open-lab.com
  *
  *  Licences: MIT, GPL
  *  http://www.opensource.org/licenses/mit-license.php
  *  http://www.gnu.org/licenses/gpl.html
+ *
+ *  last modified: 04/01/13 17.13
  *  *****************************************************************************
  */
 
 /*
  *
- * jQuery.mb.components: jquery.mb.ile
- * version: 1.0 alpha - 12-nov-2010 - 48
- * © 2001 - 2011 Matteo Bicocchi (pupunzi), Open Lab
- *
- * Licences: MIT, GPL
- * http://www.opensource.org/licenses/mit-license.php
- * http://www.gnu.org/licenses/gpl.html
- *
- * email: mbicocchi@open-lab.com
- * site: http://pupunzi.com
  *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  * Thanks to Roberto Bicchierai
